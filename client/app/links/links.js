9e84ea719f7c4c3f223195a2a9dcc1edb8a6b167
dscx <dnchmbrln@gmail.com>
2014-08-11 20:34:02 -0700

    completed karma spec and working on link shortning app functionality

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,5 +1,17 @@
 angular.module('shortly.links', [])
 
-.controller('LinksController', function ($scope, Links) {
-  // Your code here
+.controller('LinksController', function ($scope, Links, $http) {
+  
+  $scope.data = {links: []}; //refactor to use Links?
+
+  $scope.getLinks = function(){
+    $http.get("/api/links").success(function(data){
+      $scope.data.links = data;
+    });
+  };
+
+  $scope.getLinks();
 });
+
+
+  // $http.get('/someUrl').success(successCallback);
--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,5 +1,22 @@
 angular.module('shortly.shorten', [])
 
-.controller('ShortenController', function ($scope, $location, Links) {
+.controller('ShortenController', function ($scope, $location, Links, $http) {
   // Your code here
+  $scope.link = {};
+  $scope.loading = false;
+  
+
+  $scope.addLink = function (){
+    $scope.loading = true;
+    $http.post('/api/links', $scope.link).success(function(data){
+      $scope.link.url = '';
+      $scope.loading = false;
+      $scope.shortLink(data); //new url 
+    });
+
+  };
+
+  $scope.shortLink = function(data){
+    return data.url + "/api/links/" + data.code;
+  }
 });

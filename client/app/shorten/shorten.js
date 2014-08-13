447a7e4b95fc0b32ce49a0dbd8a473d8bd18d913
dscx <dnchmbrln@gmail.com>
2014-08-12 18:50:21 -0700

    added some styling with bootstrap and removed some bad code

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -12,30 +12,11 @@ angular.module('shortly.shorten', [])
       $http.post('/api/links', $scope.link).success(function(data){
         $scope.link.url = null;
         $scope.loading = false;
-        // console.log(data);
         $scope.shortLink = data; //new url 
+        $location.path("/");
       });
     }
 
   };
 
 });
-
-// app.js
-  // // create angular app
-  // var validationApp = angular.module('validationApp', []);
-
-  // // create angular controller
-  // validationApp.controller('mainController', function($scope) {
-
-  //   // function to submit the form after all validation has occurred      
-  //   $scope.submitForm = function(isValid) {
-
-  //     // check to make sure the form is completely valid
-  //     if (isValid) {
-  //       alert('our form is amazing');
-  //     }
-
-  //   };
-
-  // });
